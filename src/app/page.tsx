345fa671fbcef9c43e8ed22999f3c5b8f404fbbb
Al Nayeem <nayeem2019@hotmail.com>
2025-10-24 11:54:23 +0100

    fixed hero section responsivness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ const page = () => {
       <MyNavbar />
       <section
         id="hero"
-        className="relative bg-gray-900 min-h-screen flex items-center justify-center px-6"
+        className="relative bg-gray-900  h-screen flex items-center justify-center px-6"
       >
         {/* Background overlay / optional image */}
         <div className="absolute inset-0 -z-10">
@@ -26,12 +26,12 @@ const page = () => {
           <div className="absolute inset-0 bg-black/40"></div>
         </div>
 
-        <div className="max-w-3xl text-center z-10">
-          <h1 className="text-4xl md:text-5xl lg:text-6xl font-extrabold text-white mb-6 leading-snug tracking-tight">
-            Transform Your Home’s Efficiency with the <span className="text-green-500">ECO4 Grant</span>
+        <div className="max-w-3xl mt-5 text-center z-10">
+          <h1 className="text-3xl md:text-3xl lg:text-6xl font-extrabold text-white mb-6 leading-snug tracking-tight">
+            Transform Your Home’s Efficiency <br />with the <span className="text-green-500">ECO4 Grant</span>
           </h1>
 
-          <p className="text-gray-200 text-lg md:text-xl lg:text-2xl mb-10 leading-relaxed">
+          <p className="text-gray-200 text-lg  lg:text-2xl mb-10 leading-relaxed">
             Install a brand-new boiler, insulation, or renewable measures at{' '}
             <span className="font-semibold text-green-500">no cost</span>. Lower your bills, save energy,
             and make your home greener — fully funded by the UK ECO4 Scheme.
